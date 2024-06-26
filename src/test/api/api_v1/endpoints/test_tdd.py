0fef07f100c38dc1a4fc5c1763e506583c0d5d41
hmamiya <hmamiya@dtc.work>
2024-06-26 21:40:48 +0900

    1. add test

--- src/test/api/api_v1/endpoints/test_tdd.py
+++ src/test/api/api_v1/endpoints/test_tdd.py
@@ -4,5 +4,10 @@ import unittest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../app')))
 
+class TestArrayOperations(unittest.TestCase):
+    def test_配列を受け取りその配列を返す(self):
+        array = ArrayOperations([16,12,24])
+        assert [16,12,24] == array.raw()
+
 if __name__ == "__main__":
     unittest.main()
